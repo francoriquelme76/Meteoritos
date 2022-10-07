2f9ffb52d53cd948a30dc66e836c84e8a37207b8
Franco Riquelme <francoriquelme76@gmail.com>
2022-10-07 12:07:29 -0300

    Movimiento pulido
    Se modificaron:
    -Velocidad
    -Rotacion
    -Angular
    -Friccion

---
+++ Juego/Naves/Jugador/Player.gd
@@ -0,0 +1,31 @@
+class_name Player
+extends RigidBody2D
+
+## Atributos Export
+export var potencia_motor:int = 20
+export var potencia_rotacion:int = 200
+
+var dir_rotacion:int = 0
+var empuje:Vector2 = Vector2.ZERO
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	apply_central_impulse(empuje.rotated(rotation))
+	apply_torque_impulse(dir_rotacion * potencia_rotacion)
+
+func _process(delta: float) -> void:
+	player_input()
+
+func player_input():
+	#Empuje
+	empuje = Vector2.ZERO
+	if Input.is_action_pressed("mover_adelante"):
+		empuje = Vector2(potencia_motor, 0)
+	elif Input.is_action_pressed("mover_atras"):
+		empuje = Vector2(-potencia_motor, 0)
+	
+	#Rotacion
+	dir_rotacion = 0
+	if Input.is_action_pressed("rotar_horario"):
+		dir_rotacion += 1
+	elif Input.is_action_pressed("rotar_antihorario"):
+		dir_rotacion -= 1
